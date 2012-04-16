525409625ca6284237a68a08b8e0fb45515b76c0
UV <uvwild@gmail.com>
2012-04-16 02:31:10 +0200

    add funky social net bar

--- wp-content/themes/sight/footer.php
+++ wp-content/themes/sight/footer.php
@@ -20,6 +20,85 @@
     /> </a>
   </p>
 </div><!-- footer -->
+<div
+	class="shr-bookmarks shr-bookmarks-expand shr-bookmarks-center shr-bookmarks-bg-knowledge"
+	style="margin-left: 3.5px; height: 32px; display: block; overflow: hidden;">
+	<ul class="socials">
+		<li class="shr-facebook"><a
+			onclick="window.open(this.href,'sharer','toolbar=0,status=0,width=626,height=436'); return false;"
+			title="Share this on Facebook" class="external" rel="nofollow"
+			href="http://www.facebook.com/share.php?v=4&amp;src=bm&amp;u=<?php bloginfo('home') ?>&amp;t=<?php urlencode(bloginfo('description')) ?>"
+			target="_blank">&nbsp;</a>
+		</li>
+		<li class="shr-twitter"><a title="Tweet This!" class="external"
+			rel="nofollow"
+			href="http://twitter.com/home?status=<?php urlencode(bloginfo('description')) ?>+-+http://tinyurl.com/26sndph&amp;source=shareaholic"
+			target="_blank">&nbsp;</a>
+		</li>
+		<li class="shr-delicious"><a title="Share this on del.icio.us"
+			class="external" rel="nofollow"
+			href="http://delicious.com/post?url=<?php bloginfo('home') ?>&amp;title=<?php urlencode(bloginfo('description')) ?>"
+			target="_blank">&nbsp;</a>
+		</li>
+		<li class="shr-stumbleupon"><a
+			title="Stumble upon something good? Share it on StumbleUpon"
+			class="external" rel="nofollow"
+			href="http://www.stumbleupon.com/submit?url=<?php bloginfo('home') ?>&amp;title=<?php urlencode(bloginfo('description')) ?>"
+			target="_blank">&nbsp;</a>
+		</li>
+		<li class="shr-digg"><a title="Digg this!" class="external"
+			rel="nofollow"
+			href="http://digg.com/submit?phase=2&amp;url=<?php bloginfo('home') ?>&amp;title=<?php urlencode(bloginfo('description')) ?>"
+			target="_blank">&nbsp;</a>
+		</li>
+		<li class="shr-gmail"><a title="Email this via Gmail" class="external"
+			rel="nofollow"
+			href="https://mail.google.com/mail/?ui=2&amp;view=cm&amp;fs=1&amp;tf=1&amp;su=<?php urlencode(bloginfo('description')) ?>&amp;body=Link: <?php bloginfo('home') ?> (sent via shareaholic)%0D%0A%0D%0A----%0D%0A A%20simple%20solution%20to%20scroll%20the%20page%20so%20the%20next%20mandatory%20field%20in%20your%20form%20will%20become%20visible%20in%20case%20the%20user%20forgot%20to%20enter%20all%20the%20required%20data."
+			target="_blank">&nbsp;</a>
+		</li>
+		<li class="shr-linkedin"><a title="Share this on LinkedIn"
+			class="external" rel="nofollow"
+			href="http://www.linkedin.com/shareArticle?mini=true&amp;url=<?php bloginfo('home') ?>&amp;title=<?php urlencode(bloginfo('description')) ?>&amp;summary=A%20simple%20solution%20to%20scroll%20the%20page%20so%20the%20next%20mandatory%20field%20in%20your%20form%20will%20become%20visible%20in%20case%20the%20user%20forgot%20to%20enter%20all%20the%20required%20data.&amp;source=SolidPitch"
+			target="_blank">&nbsp;</a>
+		</li>
+		<li class="shr-googlebuzz"><a title="Post on Google Buzz"
+			class="external" rel="nofollow"
+			href="http://www.google.com/buzz/post?url=<?php bloginfo('home') ?>&amp;imageurl="
+			target="_blank">&nbsp;</a>
+		</li>
+		<li class="shr-googlebookmarks"><a
+			title="Add this to Google Bookmarks" class="external" rel="nofollow"
+			href="http://www.google.com/bookmarks/mark?op=add&amp;bkmk=<?php bloginfo('home') ?>&amp;title=<?php urlencode(bloginfo('description')) ?>"
+			target="_blank">&nbsp;</a>
+		</li>
+		<li class="shr-hotmail"><a title="Email this via Hotmail"
+			class="external" rel="nofollow"
+			href="http://mail.live.com/?rru=compose?subject=<?php urlencode(bloginfo('description')) ?>&amp;body=Link: <?php bloginfo('home') ?> (sent via shareaholic)%0D%0A%0D%0A----%0D%0A A%20simple%20solution%20to%20scroll%20the%20page%20so%20the%20next%20mandatory%20field%20in%20your%20form%20will%20become%20visible%20in%20case%20the%20user%20forgot%20to%20enter%20all%20the%20required%20data."
+			target="_blank">&nbsp;</a>
+		</li>
+		<li class="shr-technorati"><a title="Share this on Technorati"
+			class="external" rel="nofollow"
+			href="http://technorati.com/faves?add=<?php bloginfo('home') ?>"
+			target="_blank">&nbsp;</a>
+		</li>
+		<li class="shr-reddit"><a title="Share this on Reddit"
+			class="external" rel="nofollow"
+			href="http://reddit.com/submit?url=<?php bloginfo('home') ?>&amp;title=<?php urlencode(bloginfo('description')) ?>"
+			target="_blank">&nbsp;</a>
+		</li>
+		<li class="shr-diigo"><a title="Post this on Diigo" class="external"
+			rel="nofollow"
+			href="http://www.diigo.com/post?url=<?php bloginfo('home') ?>&amp;title=<?php urlencode(bloginfo('description')) ?>&amp;desc=A%20simple%20solution%20to%20scroll%20the%20page%20so%20the%20next%20mandatory%20field%20in%20your%20form%20will%20become%20visible%20in%20case%20the%20user%20forgot%20to%20enter%20all%20the%20required%20data."
+			target="_blank">&nbsp;</a>
+		</li>
+		<li class="shr-comfeed"><a
+			title="Subscribe to the comments for this post?" class="external"
+			rel="nofollow"
+			href="<?php bloginfo('home') ?>feed"
+			target="_blank">&nbsp;</a>
+		</li>
+	</ul>
+	<div style="clear: both;"></div>
 </div>
 <div id="debugDiv" class="ui-draggable resizable">
   <span id="closeButton" onClick="toggleDebugDiv();">X</span>
